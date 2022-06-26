ce782e56c32bc7078cae67a39e9da287fcd8a119
Matthew Tondreau <mmtondreau@gmail.com>
2022-06-25 17:39:24 -0700

    WIP

--- be/src/main/java/org/tonberry/calories/calorieserver/controllers/graphql/MutationController.java
+++ be/src/main/java/org/tonberry/calories/calorieserver/controllers/graphql/MutationController.java
@@ -2,27 +2,19 @@ package org.tonberry.calories.calorieserver.controllers.graphql;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
-import org.tonberry.calories.calorieserver.services.AuthService;
-import org.tonberry.graphql.Types;
 import reactor.core.publisher.Mono;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 public class MutationController {
 
-    private final AuthService authService;
 
     @MutationMapping
-    public Mono<Boolean> login(@Argument(name = "input") Types.LoginInput input) {
-        return  Mono.just(authService.authenticate(input.getUsername(), input.getPassword()));
-    }
-
-    @MutationMapping
-    public Mono<Boolean> logout() {
-        authService.deauthorize();
-        return Mono.just(true);
+    public Mono<String> login(Principal principal) {
+        return  Mono.just(principal.getName());
     }
 }
