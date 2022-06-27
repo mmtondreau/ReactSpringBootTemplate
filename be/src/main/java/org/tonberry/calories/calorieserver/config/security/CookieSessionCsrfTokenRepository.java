9ad238ffcf048c54744b417166a67c5b020c8d30
Matthew Tondreau <mmtondreau@gmail.com>
2022-06-26 22:34:57 -0700

    Use session + cookies

---
+++ be/src/main/java/org/tonberry/calories/calorieserver/config/security/CookieSessionCsrfTokenRepository.java
@@ -0,0 +1,115 @@
+package org.tonberry.calories.calorieserver.config.security;
+
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.web.server.csrf.CsrfToken;
+import org.springframework.security.web.server.csrf.WebSessionServerCsrfTokenRepository;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class CookieSessionCsrfTokenRepository extends WebSessionServerCsrfTokenRepository {
+
+    static final String DEFAULT_CSRF_COOKIE_NAME = "XSRF-TOKEN";
+    static final String DEFAULT_CSRF_PARAMETER_NAME = "_csrf";
+    static final String DEFAULT_CSRF_HEADER_NAME = "X-XSRF-TOKEN";
+
+    private String parameterName = DEFAULT_CSRF_PARAMETER_NAME;
+
+    private String headerName = DEFAULT_CSRF_HEADER_NAME;
+
+    private String cookiePath;
+
+    private String cookieDomain;
+
+    private String cookieName = DEFAULT_CSRF_COOKIE_NAME;
+
+    private boolean cookieHttpOnly = true;
+
+    private Boolean secure;
+
+    public static CookieSessionCsrfTokenRepository withHttpOnlyFalse() {
+        CookieSessionCsrfTokenRepository result = new CookieSessionCsrfTokenRepository();
+        result.setCookieHttpOnly(false);
+        return result;
+    }
+
+    public void setCookieHttpOnly(boolean cookieHttpOnly) {
+        this.cookieHttpOnly = cookieHttpOnly;
+    }
+
+    @Override
+    public Mono<Void> saveToken(ServerWebExchange exchange, CsrfToken token) {
+        return super.saveToken(exchange, token).doOnSuccess((re) -> {
+            String tokenValue = (token != null) ? token.getToken() : "";
+            // @formatter:off
+            ResponseCookie cookie = ResponseCookie
+                .from(this.cookieName, tokenValue)
+                .domain(this.cookieDomain)
+                .httpOnly(this.cookieHttpOnly)
+                .maxAge(!tokenValue.isEmpty() ? -1 : 0)
+                .path((this.cookiePath != null) ? this.cookiePath : getRequestContext(exchange.getRequest()))
+                .secure((this.secure != null) ? this.secure : (exchange.getRequest().getSslInfo() != null))
+                .build();
+            // @formatter:on
+            exchange.getResponse().addCookie(cookie);
+        });
+    }
+
+
+    public void setCookieName(String cookieName) {
+        Assert.hasLength(cookieName, "cookieName can't be null");
+        this.cookieName = cookieName;
+    }
+
+    /**
+     * Sets the parameter name
+     * @param parameterName The parameter name
+     */
+    public void setParameterName(String parameterName) {
+        Assert.hasLength(parameterName, "parameterName can't be null");
+        this.parameterName = parameterName;
+    }
+
+    /**
+     * Sets the header name
+     * @param headerName The header name
+     */
+    public void setHeaderName(String headerName) {
+        Assert.hasLength(headerName, "headerName can't be null");
+        this.headerName = headerName;
+    }
+
+    /**
+     * Sets the cookie path
+     * @param cookiePath The cookie path
+     */
+    public void setCookiePath(String cookiePath) {
+        this.cookiePath = cookiePath;
+    }
+
+    /**
+     * Sets the cookie domain
+     * @param cookieDomain The cookie domain
+     */
+    public void setCookieDomain(String cookieDomain) {
+        this.cookieDomain = cookieDomain;
+    }
+
+    /**
+     * Sets the cookie secure flag. If not set, the value depends on
+     * {@link ServerHttpRequest#getSslInfo()}.
+     * @param secure The value for the secure flag
+     * @since 5.5
+     */
+    public void setSecure(boolean secure) {
+        this.secure = secure;
+    }
+
+
+    private String getRequestContext(ServerHttpRequest request) {
+        String contextPath = request.getPath().contextPath().value();
+        return StringUtils.hasLength(contextPath) ? contextPath : "/";
+    }
+}
--- be/src/main/java/org/tonberry/calories/calorieserver/config/security/SecurityConfigurer.java
+++ be/src/main/java/org/tonberry/calories/calorieserver/config/security/SecurityConfigurer.java
@@ -11,14 +11,9 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.logout.DelegatingServerLogoutHandler;
 import org.springframework.security.web.server.authentication.logout.HttpStatusReturningServerLogoutSuccessHandler;
-import org.springframework.security.web.server.authentication.logout.WebSessionServerLogoutHandler;
 import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
-import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
-import org.springframework.security.web.server.csrf.CsrfServerLogoutHandler;
 import org.springframework.security.web.server.csrf.CsrfToken;
-import org.springframework.security.web.server.csrf.WebSessionServerCsrfTokenRepository;
 import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
@@ -40,14 +35,15 @@ public class SecurityConfigurer  {
 
     @Bean
     public SecurityWebFilterChain authorization(final ServerHttpSecurity http) {
-        final var csrfTokenRepository = new WebSessionServerCsrfTokenRepository();
-        return http.csrf(csrf -> csrf.csrfTokenRepository(csrfTokenRepository))
-                .authorizeExchange().anyExchange().permitAll()
-                .and().httpBasic(Customizer.withDefaults())
+        return http.csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieSessionCsrfTokenRepository.withHttpOnlyFalse()))
+                .authorizeExchange(ae -> ae.anyExchange().permitAll())
+                .httpBasic(Customizer.withDefaults())
                 .securityContextRepository(new WebSessionServerSecurityContextRepository())
-                .logout().logoutUrl("/logout")
-                .logoutSuccessHandler(new HttpStatusReturningServerLogoutSuccessHandler(HttpStatus.OK))
-                .and().build();
+                .logout(logoutSpec -> logoutSpec
+                        .logoutUrl("/logout")
+                        .logoutSuccessHandler(new HttpStatusReturningServerLogoutSuccessHandler(HttpStatus.OK))
+                ).build();
     }
 
     @Bean
@@ -55,5 +51,12 @@ public class SecurityConfigurer  {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    WebFilter addCsrfToken() {
+        return (exchange, next) ->
+                Objects.requireNonNull(exchange.<Mono<CsrfToken>>getAttribute(CsrfToken.class.getName()))
+                .doOnSuccess(token -> {}) // do nothing, just subscribe :/
+                .then(next.filter(exchange));
+    }
 
 }
