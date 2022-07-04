03b24e5fe743e77a4f46a32e593068f9291ca2c2
Matthew Tondreau <mmtondreau@gmail.com>
2022-07-03 18:00:10 -0700

    Make create admin transactional

--- be/src/main/java/org/tonberry/calories/calorieserver/events/ApplicationStartup.java
+++ be/src/main/java/org/tonberry/calories/calorieserver/events/ApplicationStartup.java
@@ -5,12 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.tonberry.calories.calorieserver.persistence.auth.Authority;
 import org.tonberry.calories.calorieserver.persistence.auth.Role;
 import org.tonberry.calories.calorieserver.persistence.auth.User;
-import org.tonberry.calories.calorieserver.repository.UserRepository;
 import org.tonberry.calories.calorieserver.services.UserService;
 import reactor.core.publisher.Mono;
 
@@ -18,12 +17,7 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @RequiredArgsConstructor
 public class ApplicationStartup {
-
-
-    private final UserRepository userRepository;
-
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
     @Value("${admin.password}")
     String adminPassword;
     @Value("${admin.username}")
@@ -33,15 +27,22 @@ public class ApplicationStartup {
 
     @EventListener(ApplicationReadyEvent.class)
     public void applicationReadyEvent() {
-        if (userService.findByUsername(adminUsername).block() == null) {
-            log.info("Creating admin [%s]".formatted(adminUsername));
-            Mono<Role> role = userService.createRole("admin");
-            Mono<Authority> authority = userService.createAuthority("ROLE_ADMIN");
-            Mono<User> user = userService.createUser(adminUsername, adminPassword);
-            Mono.zip(role, authority, user).flatMap(result -> Mono.zip(
-                    userService.addRoleToUser(result.getT1(), result.getT3()),
-                    userService.addAuthorityToRole(result.getT2(), result.getT1()))
-            ).block();
+        User user = userService.findByUsername(adminUsername).block();
+        if (user == null) {
+            createAdminUser().block();
         }
     }
+
+    @Transactional
+    public Mono<Object> createAdminUser() {
+        log.info("Creating admin [%s]".formatted(adminUsername));
+        Mono<User> user = userService.createUser(adminUsername, adminPassword);
+        Mono<Role> role = userService.createRole("admin");
+        Mono<Authority> authority = userService.createAuthority("ROLE_ADMIN");
+        return Mono.zip(role, authority, user).flatMap(result -> Mono.zip(
+                userService.addRoleToUser(result.getT1(), result.getT3()),
+                userService.addAuthorityToRole(result.getT2(), result.getT1()))
+        );
+    }
+
 }
